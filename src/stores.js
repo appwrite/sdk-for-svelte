1df04ea0154f948c6bae962a44c7207d4a18f62f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-03-29 19:39:00 +0200

    fix uppercase in import

--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
-import { UserStore } from "./Stores/user";
-import { DocumentsStore } from "./Stores/documents";
+import { UserStore } from "./stores/user";
+import { DocumentsStore } from "./stores/documents";
 
 export const active = writable(false);
 export const currentUser = new UserStore();
